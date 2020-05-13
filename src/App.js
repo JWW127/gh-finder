8f2b5047a88b34bb2462240861f0c8e6c95dbb86
JWW127 <joseph.w.woolever@gmail.com>
2020-05-13 00:35:31 -0500

    Added router ability and about page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
 import Search from './components/users/Search'
+import About from './components/pages/About'
 import axios from 'axios'
 import './App.css'
 
@@ -11,19 +13,11 @@ class App extends Component {
         loading: false,
     }
 
-    // async componentDidMount() {
-    //     this.setState({
-    //         loading: true,
-    //     })
-
-    //     const res = await axios.get(
-    //         `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_Id}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    //     )
-
-    //     this.setState({ users: res.data, loading: false })
-    // }
-
     searchUsers = async text => {
+        this.setState({
+            loading: true,
+        })
+
         const res = await axios.get(
             `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_Id}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
         )
@@ -33,16 +27,27 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <Navbar />
-                <div className="container">
-                    <Search searchUsers={this.searchUsers} />
-                    <Users
-                        loading={this.state.loading}
-                        users={this.state.users}
-                    />
+            <Router>
+                <div className="App">
+                    <Navbar />
+                    <div className="container">
+                        <Route
+                            exact
+                            path="/"
+                            render={props => (
+                                <Fragment>
+                                    <Search searchUsers={this.searchUsers} />
+                                    <Users
+                                        loading={this.state.loading}
+                                        users={this.state.users}
+                                    />
+                                </Fragment>
+                            )}
+                        />
+                        <Route exact path="/about" component={About} />
+                    </div>
                 </div>
-            </div>
+            </Router>
         )
     }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react'
+
+export const About = () => {
+    return (
+        <Fragment>
+            <h1>About</h1>
+            <h3>What is this App?</h3>
+            <ul>
+                <li>Search All of Github Registered Users</li>
+                <li>Return Detailed User Information</li>
+                <li>Version 1.0.0</li>
+            </ul>
+        </Fragment>
+    )
+}
+
+export default About
