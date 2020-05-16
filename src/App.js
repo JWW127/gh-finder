9d5cfecad99303219bdda94f0ba8d4cd5904b8de
JWW127 <joseph.w.woolever@gmail.com>
2020-05-15 22:42:15 -0500

    add individual user pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import User from './components/users/User'
 import Search from './components/users/Search'
 import About from './components/pages/About'
 import axios from 'axios'
@@ -10,6 +11,7 @@ import './App.css'
 class App extends Component {
     state = {
         users: [],
+        user: {},
         loading: false,
     }
 
@@ -25,26 +27,55 @@ class App extends Component {
         this.setState({ users: res.data.items, loading: false })
     }
 
+    //get single user
+    getUser = async username => {
+        this.setState({ loading: true })
+
+        const res = await axios.get(
+            `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_Id}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        )
+
+        this.setState({ user: res.data, loading: false })
+    }
+
     render() {
+        const { users, user, loading } = this.state
+
         return (
             <Router>
                 <div className="App">
                     <Navbar />
                     <div className="container">
-                        <Route
-                            exact
-                            path="/"
-                            render={props => (
-                                <Fragment>
-                                    <Search searchUsers={this.searchUsers} />
-                                    <Users
-                                        loading={this.state.loading}
-                                        users={this.state.users}
+                        <Switch>
+                            <Route
+                                exact
+                                path="/"
+                                render={props => (
+                                    <Fragment>
+                                        <Search
+                                            searchUsers={this.searchUsers}
+                                        />
+                                        <Users
+                                            loading={loading}
+                                            users={users}
+                                        />
+                                    </Fragment>
+                                )}
+                            />
+                            <Route exact path="/about" component={About} />
+                            <Route
+                                exact
+                                path="/user/:login"
+                                render={props => (
+                                    <User
+                                        {...props}
+                                        getUser={this.getUser}
+                                        user={user}
+                                        loading={loading}
                                     />
-                                </Fragment>
-                            )}
-                        />
-                        <Route exact path="/about" component={About} />
+                                )}
+                            />
+                        </Switch>
                     </div>
                 </div>
             </Router>
