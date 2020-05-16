e7e0a328e0a8b01f7faf6cb39bdee9542559af3a
JWW127 <joseph.w.woolever@gmail.com>
2020-05-16 02:55:44 -0500

    added repos to profiles

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
     state = {
         users: [],
         user: {},
+        repos: [],
         loading: false,
     }
 
@@ -38,8 +39,19 @@ class App extends Component {
         this.setState({ user: res.data, loading: false })
     }
 
+    //get user's repos
+    getUserRepos = async username => {
+        this.setState({ loading: true })
+
+        const res = await axios.get(
+            `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_Id}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        )
+
+        this.setState({ repos: res.data, loading: false })
+    }
+
     render() {
-        const { users, user, loading } = this.state
+        const { users, user, loading, repos } = this.state
 
         return (
             <Router>
@@ -70,7 +82,9 @@ class App extends Component {
                                     <User
                                         {...props}
                                         getUser={this.getUser}
+                                        getUserRepos={this.getUserRepos}
                                         user={user}
+                                        repos={repos}
                                         loading={loading}
                                     />
                                 )}
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,17 +1,21 @@
 import React, { Component, Fragment } from 'react'
 import Spinner from '../layout/Spinner'
+import Repos from '../repos/Repos'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 export class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login)
+        this.props.getUserRepos(this.props.match.params.login)
     }
 
     static propTypes = {
         loading: PropTypes.bool,
         user: PropTypes.object.isRequired,
         getUser: PropTypes.func.isRequired,
+        getUserRepos: PropTypes.func.isRequired,
+        repos: PropTypes.array.isRequired,
     }
 
     render() {
@@ -26,12 +30,12 @@ export class User extends Component {
             company,
             followers,
             following,
-            public_repose,
-            public_gist,
+            public_repos,
+            public_gists,
             hireable,
         } = this.props.user
 
-        const { loading } = this.props
+        const { loading, repos } = this.props
 
         if (loading) return <Spinner />
 
@@ -100,10 +104,13 @@ export class User extends Component {
                         Following: {following}
                     </div>
                     <div className="badge badge-danger">
-                        Public Repos: {public_repose}
+                        Public Repos: {public_repos}
                     </div>
-                    <div className="badge badge-caution">Public Gist: {public_gist}</div>
+                    <div className="badge badge-caution">
+                        Public Gist: {public_gists}
+                    </div>
                 </div>
+                <Repos repos={repos} />
             </Fragment>
         )
     }
